01b2d48cf5ecc2a68f4b44a6eb46321a83d9006e
Walt <wpniederer@cpp.edu>
2018-09-14 22:25:12 -0700

    Added skeleton methods

---
+++ src/EightPuzzle.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class EightPuzzle {
+
+    private boolean isSolvable(int[] puzzle) {
+
+        return true;
+    }
+
+    private int[][] generatePuzzles(int numberOfPuzzles) {
+        int[][] generatedPuzzles = new int[numberOfPuzzles][9];
+        return generatedPuzzles;
+    }
+
+    private void printTable() {
+
+    }
+
+    private HashMap<Integer, ArrayList<Integer>>aStar(boolean showOptSeq) {
+        // maybe....
+        HashMap<Integer, ArrayList<Integer>> solutionDepAndSearchCost = new HashMap<Integer, ArrayList<Integer>>();
+        return solutionDepAndSearchCost;
+    }
+
+    public static void main(String [] args) {
+    }
+
+}
