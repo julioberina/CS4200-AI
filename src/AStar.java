ad9f7a314c20e9c247126255613ff8117c18d1f3
Walt <wpniederer@cpp.edu>
2018-09-19 21:10:15 -0700

    refactoring classes to better work with A* and graphs

--- src/AStar.java
+++ src/AStar.java
@@ -1,67 +1,30 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class AStar {
+    private EightPuzzle puzzle;
 
-    public int hammingH1(int[] puzzle) {
-        int misplacedTiles = 0;
-        for (int i = 0; i < puzzle.length; i++) {
-            if (puzzle[i] != i && puzzle[i] != 0) {
-                misplacedTiles += 1;
-            }
-        }
-        return misplacedTiles;
+    public AStar(EightPuzzle puzzle) {
+        this.puzzle = puzzle;
     }
 
-    public static int[] getCorrectPosition (int num) {
-        int iteration = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (num == iteration) {
-                    int[] pos = {i, j};
-                    System.out.println(pos[0] + " " + pos[1]);
-                    return pos;
-                }
-                iteration += 1;
-            }
-        }
-        return null;
+    public Iterable<EightPuzzle> optimalSequence() {
+        List<EightPuzzle> sequence = new ArrayList<EightPuzzle>();
+
+        return sequence;
     }
 
-    public int manhattanH2(int[] puzzle) {
-        // Converting puzzle to 2d array to calculate manhattan dist
-        int num = 0;
-        int[][] puzzle2d = new int[3][3];
-        for (int i = 0; i < puzzle2d.length; i++) {
-            for (int j = 0; j < puzzle2d[i].length; j++) {
-                puzzle2d[i][j] = puzzle[num];
-                num += 1;
-            }
-        }
+    public int minMoves() {
+        int min = 0;
 
-        num = 0;
-        int sumOfManhattan = 0;
-        for (int y = 0; y < puzzle2d.length; y++) {
-            for (int x = 0; x < puzzle2d[y].length; x++) {
-                if (puzzle2d[y][x] != num && puzzle2d[y][x] != 0) {
-                    int[] correctPosition = getCorrectPosition(puzzle2d[y][x]);
-                    int manhattanDist = Math.abs(correctPosition[1] - x) + Math.abs(correctPosition[0] - y);
-                    sumOfManhattan += manhattanDist;
-                }
-                num += 1;
-            }
-        }
-        return sumOfManhattan;
+        return min;
     }
 
     // TODO: Implement A* with both heurstics
-    // will utilize A*
-    private HashMap<Integer, ArrayList<Integer>> eightPuzzleSolver(int[] puzzle, boolean showOptSeq) {
+    private HashMap<Integer, ArrayList<Integer>> eightPuzzleSolver() {
         // maybe....
         HashMap<Integer, ArrayList<Integer>> solutionDepAndSearchCost = new HashMap<Integer, ArrayList<Integer>>();
         return solutionDepAndSearchCost;
-    }
-    public void solver(int[] puzzle) {
-
     }
 }
--- src/EightPuzzle.java
+++ src/EightPuzzle.java
@@ -2,10 +2,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.List;
 
+// NOTE: When applying h1 and h2 they should both have the same solution depth
+// Solution cost should be close to the table
+// Catch bad input
+// h2 should be worse than h1
+// Can use graph search w/h2
 public class EightPuzzle {
+    private int[] puzzle;
 
-    private boolean isSolvable(int[] puzzle) {
+    public EightPuzzle (int[] puzzle) {
+        this.puzzle = puzzle;
+    }
+    public boolean isSolvable() {
         // Finding number of inversions
         int numberOfInversions = 0;
         for (int i = 0; i < puzzle.length; i++) {
@@ -31,13 +41,17 @@ public class EightPuzzle {
 
     }
 
-    private boolean isSolved(int[] puzzle) {
+    public boolean isSolved() {
         for (int i = 0; i < puzzle.length; i++) {
             if (i != puzzle[i]) {
                 return false;
             }
         }
         return true;
+    }
+
+    public int getTileAt(int location) {
+        return puzzle[location - 1];
     }
 
     private static void shufflePuzzle(int[] puzzle) {
@@ -52,7 +66,7 @@ public class EightPuzzle {
         }
     }
 
-    private int[] generatePuzzle() {
+    private static int[] generatePuzzle() {
        int[] generatedPuzzle = new int[9];
        for (int i = 0; i < generatedPuzzle.length; i++) {
            generatedPuzzle[i] = i;
@@ -62,7 +76,7 @@ public class EightPuzzle {
     }
 
     // Might not use
-    private int[][] generatePuzzles(int numberOfPuzzles) {
+    private static int[][] generatePuzzles(int numberOfPuzzles) {
         int[][] generatedPuzzles = new int[numberOfPuzzles][9];
         for (int i = 0; i < generatedPuzzles.length; i++) {
             for (int j = 0; j < generatedPuzzles[i].length; j++) {
@@ -111,41 +125,97 @@ public class EightPuzzle {
         System.out.format("+---+---------+-------+%n");
     }
 
+    public int hammingH1() {
+        int hamming = 0;
+        for (int i = 0; i < puzzle.length; i++) {
+            if (puzzle[i] != i && puzzle[i] != 0) {
+                hamming += 1;
+            }
+        }
+        return hamming;
+    }
+
+    public int manhattanH2() {
+        // Converting puzzle to 2d array to calculate manhattan dist
+        int num = 0;
+        int[][] puzzle2d = new int[3][3];
+        for (int i = 0; i < puzzle2d.length; i++) {
+            for (int j = 0; j < puzzle2d[i].length; j++) {
+                puzzle2d[i][j] = puzzle[num];
+                num += 1;
+            }
+        }
+
+        num = 0;
+        int sumOfManhattan = 0;
+        for (int y = 0; y < puzzle2d.length; y++) {
+            for (int x = 0; x < puzzle2d[y].length; x++) {
+                if (puzzle2d[y][x] != num && puzzle2d[y][x] != 0) {
+                    int[] correctPosition = getCorrectPosition(puzzle2d[y][x]);
+                    int manhattanDist = Math.abs(correctPosition[1] - x) + Math.abs(correctPosition[0] - y);
+                    sumOfManhattan += manhattanDist;
+                }
+                num += 1;
+            }
+        }
+        return sumOfManhattan;
+    }
+
+    private static int[] getCorrectPosition(int num) {
+        int iteration = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (num == iteration) {
+                    int[] pos = {i, j};
+//                    System.out.println(pos[0] + " " + pos[1]);
+                    return pos;
+                }
+                iteration += 1;
+            }
+        }
+        return null;
+    }
+
+    public Iterable<EightPuzzle> neighboringBoards() {
+        List<EightPuzzle> neighbors = new ArrayList<EightPuzzle>();
+        return neighbors;
+    }
+
+    // TODO: Get total number of lines in file, read in all lines skipping depth info
+    private int [][] fileReader(String fileName) {
+        int numOfLines = 0;
+        int[][] readPuzzles = new int[numOfLines][9];
+
+        return readPuzzles;
+    }
 
     public static void main(String [] args) {
-        EightPuzzle test = new EightPuzzle();
         int[] testPuzzle = {3, 1, 2, 6, 4, 5, 0, 7, 8};
         int[] testPuzzle1 = {3, 1,  2, 4, 0, 5, 6, 7, 8};
         int[] testPuzzled20 = {0, 5, 8, 2, 7, 6, 1, 3, 4};
         int[] testPuzzle3 = {7, 2, 4, 5, 0, 6, 8, 3, 1};
         int[] solvedPuzzle = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        EightPuzzle test = new EightPuzzle(testPuzzle3);
 
         System.out.println("Checking if solvable");
-        System.out.println(test.isSolvable(testPuzzle));
-        System.out.println(test.isSolvable(testPuzzle1));
-        System.out.println(test.isSolvable(testPuzzled20));
-        System.out.println(test.isSolvable(testPuzzle3));
-        System.out.println(test.isSolvable(solvedPuzzle));
+        System.out.println(test.isSolvable());
         System.out.println();
 
         System.out.println("Checking if solved");
-        System.out.println(test.isSolved(testPuzzle));
-        System.out.println(test.isSolved(testPuzzle1));
-        System.out.println(test.isSolved(testPuzzled20));
-        System.out.println(test.isSolved(testPuzzle3));
-        System.out.println(test.isSolved(solvedPuzzle));
+        System.out.println(test.isSolved());
+        System.out.println();
+
+        System.out.println("Checking getTileAt");
+        System.out.println(test.getTileAt(4));
         System.out.println();
 
         // Testing puzzle generator
-        int[][] testPuzzles = test.generatePuzzles(100);
-        int[] testGeneratedPuzzle = test.generatePuzzle();
+        int[][] testPuzzles = generatePuzzles(100);
+        int[] testGeneratedPuzzle = generatePuzzle();
 
         for (int num : testGeneratedPuzzle) {
             System.out.print(num + " ");
         }
-        System.out.println();
-        System.out.println(test.isSolvable(testGeneratedPuzzle));
-        System.out.println(test.isSolved(testGeneratedPuzzle));
 
 
         // Testing printTable
@@ -161,14 +231,10 @@ public class EightPuzzle {
         System.out.println();
 
         System.out.println("Checking hamming");
-        AStar aTest = new AStar();
-        System.out.println(aTest.hammingH1(testPuzzle));
-        System.out.println(aTest.hammingH1(testPuzzle1));
-        System.out.println(aTest.hammingH1(testPuzzled20));
-        System.out.println(aTest.hammingH1(testPuzzle3));
-        System.out.println(aTest.hammingH1(solvedPuzzle));
+        System.out.println(test.hammingH1());
 
-        System.out.println(aTest.manhattanH2(testPuzzle3));
+        System.out.println("Checking manhattan");
+        System.out.println(test.manhattanH2());
         System.out.println();
 
 
