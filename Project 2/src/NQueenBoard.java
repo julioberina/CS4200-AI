aa742779021d0c31b93a0376d78c5448a79d4669
Walt <wpniederer@cpp.edu>
2018-10-02 11:00:15 -0700

    skeleton methods for all classes

--- Project 2/src/NQueenBoard.java
+++ Project 2/src/NQueenBoard.java
@@ -1,2 +1,28 @@
 public class NQueenBoard {
+    private String board;
+    private int size;
+
+    public NQueenBoard() {
+
+    }
+
+    public NQueenBoard(String board) {
+        this.board = board;
+        this.size = board.length();
+    }
+
+    public void moveQueens() {
+
+    }
+
+    public int numberOfAttackingQueens() {
+        int numberOfAttackers = 0;
+
+        return numberOfAttackers;
+    }
+
+    public boolean isSolved() {
+        return false;
+    }
+
 }
--- Project 2/src/SimulatedAnnealing.java
+++ Project 2/src/SimulatedAnnealing.java
@@ -1,2 +1,22 @@
 public class SimulatedAnnealing {
+    private int cost;
+    private long runtime;
+    private boolean isSolved;
+;
+
+    public SimulatedAnnealing(NQueenBoard board) {
+
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public long getRuntime() {
+        return runtime;
+    }
+
+    public boolean isSolved() {
+        return isSolved;
+    }
 }
