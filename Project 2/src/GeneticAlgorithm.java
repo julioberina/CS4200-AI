5440dec33b2deea6797cd8da51e10056cef770f1
Walt <wpniederer@cpp.edu>
2018-10-02 11:00:15 -0700

    skeleton methods for all classes

--- Project 2/src/GeneticAlgorithm.java
+++ Project 2/src/GeneticAlgorithm.java
@@ -1,2 +1,21 @@
 public class GeneticAlgorithm {
+    private int cost;
+    private long runtime;
+    private boolean isSolved;
+
+    public GeneticAlgorithm(NQueenBoard board) {
+
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public long getRuntime() {
+        return runtime;
+    }
+
+    public boolean isSolved() {
+        return isSolved;
+    }
 }
--- Project 2/src/Main.java
+++ Project 2/src/Main.java
@@ -1,6 +1,43 @@
 public class Main {
 
+    public String generateBoard(int size) {
+        String generatedBoard = "";
+
+        return generatedBoard;
+    }
+
+    public void AnalyzeAlgorithm(boolean useGenetic, int numberOfPuzzles) {
+        int solvedProblems, searchCostTotal, runtimesTotal;
+        solvedProblems = searchCostTotal = runtimesTotal = 0;
+        String board;
+        GeneticAlgorithm geneticAlgorithm;
+        SimulatedAnnealing simulatedAnnealing;
+        int size = 21;
+        for (int i = 1; i <= numberOfPuzzles; i++) {
+            board = generateBoard(size);
+
+            if (useGenetic) {
+                geneticAlgorithm = new GeneticAlgorithm(new NQueenBoard(board));
+                if (geneticAlgorithm.isSolved()) {
+                    solvedProblems += 1;
+                }
+                searchCostTotal += geneticAlgorithm.getCost();
+                runtimesTotal += geneticAlgorithm.getRuntime();
+
+            } else {
+                simulatedAnnealing = new SimulatedAnnealing(new NQueenBoard(board));
+                if (simulatedAnnealing.isSolved()) {
+                    solvedProblems += 1;
+                }
+                searchCostTotal += simulatedAnnealing.getCost();
+                runtimesTotal += simulatedAnnealing.getRuntime();
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
+
 }
