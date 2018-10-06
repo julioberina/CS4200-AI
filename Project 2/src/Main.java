190ff683c866cbfc59822bd808e91f96912fc9e9
Walt <wpniederer@cpp.edu>
2018-10-06 02:54:30 -0700

    finished logic for getting attacking queen pairs

--- Project 2/src/Main.java
+++ Project 2/src/Main.java
@@ -1,20 +1,25 @@
+import java.util.Random;
+
 public class Main {
 
-    public String generateBoard(int size) {
-        String generatedBoard = "";
-
+    public static int[] generateBoard(int size) {
+        int[] generatedBoard = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < generatedBoard.length; i++) {
+            int n = rand.nextInt(size);
+            generatedBoard[i] = n;
+        }
         return generatedBoard;
     }
 
-    public void AnalyzeAlgorithm(boolean useGenetic, int numberOfPuzzles) {
+    public void algorithmAnalysis(boolean useGenetic, int numberOfPuzzles, int sizeOfPuzzle) {
         int solvedProblems, searchCostTotal, runtimesTotal;
         solvedProblems = searchCostTotal = runtimesTotal = 0;
-        String board;
+        int[] board;
         GeneticAlgorithm geneticAlgorithm;
         SimulatedAnnealing simulatedAnnealing;
-        int size = 21;
         for (int i = 1; i <= numberOfPuzzles; i++) {
-            board = generateBoard(size);
+            board = generateBoard(sizeOfPuzzle);
 
             if (useGenetic) {
                 geneticAlgorithm = new GeneticAlgorithm(new NQueenBoard(board));
@@ -34,10 +39,38 @@ public class Main {
             }
         }
 
+        System.out.println("Averages for board of size " + sizeOfPuzzle);
+        System.out.println("Percentage of puzzles solved: " + solvedProblems / numberOfPuzzles);
+        System.out.println("Average search cost: " + searchCostTotal/ numberOfPuzzles);
+        System.out.println("Average runtime: " + runtimesTotal/ numberOfPuzzles);
     }
 
     public static void main(String[] args) {
-	// write your code here
+// testing board generator
+//        int[] testBoards;
+//        int i = 0;
+//        while (i <= 50) {
+//            testBoards = generateBoard(21);
+//            System.out.print("Test board " + i +   "  of size " + testBoards.length + ": ");
+//            for (int num : testBoards) {
+//                System.out.print(num + " ");
+//            }
+//            System.out.println();
+//            i++;
+//        }
+        int[] testboard;
+        int[] eightBoard = {3, 2, 1, 4, 3, 2, 1, 2};
+//        int i = 0;
+//        while (i <= 200) {
+//            testboard = generateBoard(500);
+//            NQueenBoard test = new NQueenBoard(testboard);
+//            test.numberOfAttackingQueens();
+//            i++;
+//        }
+        NQueenBoard test = new NQueenBoard(eightBoard);
+        test.numberOfAttackingQueens();
+        System.out.println();
     }
+
 
 }
--- Project 2/src/NQueenBoard.java
+++ Project 2/src/NQueenBoard.java
@@ -1,28 +1,80 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class NQueenBoard {
-    private String board;
-    private int size;
+    private int[] board;
+//    private int size;
+//    private int numberOfQueens;
 
-    public NQueenBoard() {
-
-    }
-
-    public NQueenBoard(String board) {
+    public NQueenBoard(int[] board) {
         this.board = board;
-        this.size = board.length();
+//        this.size = board.length;
+//        this.numberOfQueens = board.length;
     }
 
     public void moveQueens() {
 
     }
 
-    public int numberOfAttackingQueens() {
+    // column = index, row = value
+    public HashMap<String, ArrayList<Integer[]>> numberOfAttackingQueens() {
         int numberOfAttackers = 0;
+        HashMap<String, ArrayList<Integer[]>> attackingQueenPairs = new HashMap<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = i + 1; j < board.length; j++) {
+                int elem1 = board[i];
+                int elem2 = board[j];
 
-        return numberOfAttackers;
+                // Checking if on the same row
+                if (elem1 == elem2) {
+                    numberOfAttackers++;
+                    addPair(attackingQueenPairs, i, j, elem1, elem2);
+
+                // Finding diagonal rows
+                } else {
+                    int deltaCol = calculateDifference(i, j);
+                    int deltaRow = calculateDifference(elem1, elem2);
+                    if (deltaCol == deltaRow) {
+                        numberOfAttackers++;
+                        addPair(attackingQueenPairs, i, j, elem1, elem2);
+                    }
+
+                }
+
+
+            }
+        }
+        System.out.println("Number of attackers: " + numberOfAttackers);
+        return attackingQueenPairs;
     }
+
+    private static void addPair(HashMap<String, ArrayList<Integer[]>> attackingQueenPairs, int queenOneCol, int queenTwoCol, int queenOneRow, int queenTwoRow) {
+        ArrayList<Integer[]> queen;
+        if (attackingQueenPairs.containsKey(Arrays.toString(new Integer[]{queenOneCol, queenOneRow}))) {
+            queen = attackingQueenPairs.get(Arrays.toString(new Integer[]{queenOneCol, queenOneRow}));
+            queen.add(new Integer[] {queenTwoCol, queenTwoRow});
+            attackingQueenPairs.put(Arrays.toString(new Integer[]{queenOneCol, queenOneRow}), queen);
+        } else {
+            queen = new ArrayList<>();
+            queen.add(new Integer[] {queenTwoCol, queenTwoRow});
+            attackingQueenPairs.put(Arrays.toString(new Integer[]{queenOneCol, queenOneRow}), queen);
+        }
+    }
+
+    public int calculateDifference(int a, int b) {
+        return  Math.abs(a - b);
+    }
+
 
     public boolean isSolved() {
         return false;
+    }
+
+    @Override
+    public String toString() {
+
+        return "test";
     }
 
 }
