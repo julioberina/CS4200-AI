2b1f3252c9b47fdcf9c353929709b370116f4c01
Walt <wpniederer@cpp.edu>
2018-10-10 17:47:59 -0700

    fleshing out methods needed for algorithm

--- Project 2/src/GeneticAlgorithm.java
+++ Project 2/src/GeneticAlgorithm.java
@@ -1,9 +1,32 @@
-public class GeneticAlgorithm {
+import java.util.PriorityQueue;
+
+
+public class GeneticAlgorithm implements Comparable<NQueenBoard>{
     private int cost;
     private long runtime;
-    private boolean isSolved;
+    private NQueenBoard currentBoard;
+    private NQueenBoard solutionBoard;
 
+    // keep individuals sorted by fitness in a pq
     public GeneticAlgorithm(NQueenBoard board) {
+        this.currentBoard = board;
+
+
+    }
+
+    @Override
+    // Calculating and sorting by (fitness = attacking Queen pairs)
+    public int compareTo(NQueenBoard o) {
+        int priority1 = currentBoard.totalNumberOfAttackingQueens();
+        int priority2 = o.totalNumberOfAttackingQueens();
+
+        if (priority1 < priority2) {
+            return -1;
+        } else if (priority1 > priority2) {
+            return 1;
+        } else {
+            return 0;
+        }
 
     }
 
@@ -16,6 +39,30 @@ public class GeneticAlgorithm {
     }
 
     public boolean isSolved() {
-        return isSolved;
+        return solutionBoard.totalNumberOfAttackingQueens() == 0;
+    }
+
+    public NQueenBoard generateChild(NQueenBoard mom, NQueenBoard dad) {
+        NQueenBoard child;
+
+        return child;
+    }
+
+    public void mutate(NQueenBoard child) {
+
+    }
+
+    public int getFitness() {
+        return currentBoard.totalNumberOfAttackingQueens();
+    }
+
+    public NQueenBoard getSolutionBoard() {
+        return solutionBoard;
+    }
+
+    public NQueenBoard[] generatePopulation() {
+        NQueenBoard[] population;
+
+        return population;
     }
 }
