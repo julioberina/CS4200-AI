76f1993ae5414ce8af6872e10b9e6c90697bbc02
Walt <wpniederer@cpp.edu>
2018-10-12 17:36:26 -0700

    method 3 solutions from each algorithm

--- Project 2/src/Main.java
+++ Project 2/src/Main.java
@@ -48,23 +48,59 @@ public class Main {
         }
     }
 
+    public static void get3Solutions(int sizeOfPuzzle) {
+        SimulatedAnnealing simulatedAnnealing;
+        NQueenBoard board;
+        GeneticAlgorithm genetic;
+
+        System.out.println("Simulated Annealing");
+        System.out.println("============================================================");
+        for (int i = 1; i <= 3; i++) {
+            board = new NQueenBoard(generateBoard(21));
+            System.out.println("Initial board: ");
+            System.out.println(board);
+
+            System.out.println();
+
+            simulatedAnnealing = new SimulatedAnnealing(board);
+            System.out.println("Solved board: ");
+            System.out.println(simulatedAnnealing.getSolutionBoard());
+        }
+        System.out.println("\n\n");
+
+        System.out.println("Genetic");
+        System.out.println("============================================================");
+        for (int i = 1; i <= 3; i++) {
+            genetic = new GeneticAlgorithm(21);
+            System.out.println("Initial board (1st board in pop pq): ");
+            System.out.println(genetic.getInitialBoard());
+
+            System.out.println("Solved board: ");
+            System.out.println(genetic.getSolutionBoard());
+
+        }
+
+    }
     public static void main(String[] args) {
 
 
-        System.out.println("Analysis of Simulated Annealing: ");
-        long startTime = System.currentTimeMillis();
-        algorithmAnalysis(false, 500, 21);
-        long totalTime = System.currentTimeMillis() - startTime;
-        System.out.println("Total time: " + totalTime);
 
-        System.out.println("\n");
+//        System.out.println("Analysis of Simulated Annealing: ");
+//        long startTime = System.currentTimeMillis();
+//        algorithmAnalysis(false, 500, 21);
+//        long totalTime = System.currentTimeMillis() - startTime;
+//        System.out.println("Total time: " + totalTime);
+//
+//        System.out.println("\n");
+//
+//        System.out.println("Analysis of Genetic: ");
+//        startTime = System.currentTimeMillis();
+//        algorithmAnalysis(true, 500, 21);
+//        totalTime = System.currentTimeMillis() - startTime;
+//        System.out.println("Total time: " + totalTime);
 
-        System.out.println("Analysis of Genetic: ");
-        startTime = System.currentTimeMillis();
-        algorithmAnalysis(true, 500, 21);
-        totalTime = System.currentTimeMillis() - startTime;
-        System.out.println("Total time: " + totalTime);
 
+        get3Solutions(21);
     }
 
 
