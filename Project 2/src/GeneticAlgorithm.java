26f6996fe1a495369cc1cad8aac1247d03a2cb75
Walt <wpniederer@cpp.edu>
2018-10-12 17:03:42 -0700

    further optimization of genetic by reducing size of population

--- Project 2/src/GeneticAlgorithm.java
+++ Project 2/src/GeneticAlgorithm.java
@@ -7,47 +7,34 @@ public class GeneticAlgorithm {
     private int sizeOfPuzzle;
     private NQueenBoard solutionBoard;
 
-    // keep individuals sorted by fitness in a pq
     public GeneticAlgorithm(int sizeOfPuzzle) {
         this.sizeOfPuzzle = sizeOfPuzzle;
-        int populationSize = 200;
+        int populationSize = 20;
 
         PriorityQueue<NQueenBoard> population = new PriorityQueue<>(generatePopulation(populationSize));
         ArrayList<NQueenBoard> newPopulation;
         ArrayList<NQueenBoard> topXPercent;
+        Random rand = new Random();
 
         double percentToGrab = .30;
         int elites = Math.toIntExact(Math.round(population.size() * percentToGrab));
 
         long startTimeOut = System.currentTimeMillis();
-        long timeOut = startTimeOut + 60 * 1000;
-
-//        double probabilityOfMutation = .20;
-        Random rand = new Random();
+        long timeOut = startTimeOut + 2 * 1000;
 
         NQueenBoard current = population.peek();
 
-        // better algorithm.....
-        // get top 30% of the population, select parents randomly and reproduce
-        //      need an arraylist for selecting parents randomly
-        //      pq with top %30
+        long startTime = System.currentTimeMillis();
         while (System.currentTimeMillis() < timeOut && !current.isSolved()) {
             newPopulation = new ArrayList<>();
             topXPercent = new ArrayList<>();
-            System.out.println("size of Population: " + population.size());
 
             for (int i = 0; i < elites; i++) {
                 topXPercent.add(population.poll());
             }
 
-
-//            if (population.size() % 2 != 0) {
-//                population.poll();
-//            }
-
             for (int i = 0; i < populationSize; i++) {
 
-                // might grab the same individual....
                 NQueenBoard mom = topXPercent.get(rand.nextInt(topXPercent.size()));
                 NQueenBoard dad = topXPercent.get(rand.nextInt(topXPercent.size()));
                 NQueenBoard child = generateChild(mom, dad);
@@ -55,16 +42,17 @@ public class GeneticAlgorithm {
                 if (child.isSolved()) {
                     current = child;
                     break;
+                } else {
+                    newPopulation.add(child);
                 }
 
-                newPopulation.add(child);
             }
 
             population = new PriorityQueue<>(newPopulation);
-            System.out.println("size of Population after new pop: " + population.size() + "\n");
         }
 
-        solutionBoard = population.poll();
+        runtime = System.currentTimeMillis() - startTime;
+        solutionBoard = current;
     }
 
     public int getCost() {
@@ -80,6 +68,7 @@ public class GeneticAlgorithm {
     }
 
     public NQueenBoard generateChild(NQueenBoard mom, NQueenBoard dad) {
+        cost += 1;
         Random rand = new Random();
         int[] momBoard = mom.getBoard();
         int[] dadBoard = dad.getBoard();
@@ -94,24 +83,9 @@ public class GeneticAlgorithm {
             }
         }
 
-//        NQueenBoard child;
-//        System.out.println("Crossover value: " + crossover);
-//        System.out.println("Mom: ");
-//        System.out.println(mom.toString());
-//        System.out.println("Dad: " );
-//        System.out.println(dad.toString());
-
-//        child = new NQueenBoard(childBoard);
-//        System.out.println("Child before mute: ");
-//        System.out.println(child.toString());
-//        child = mutate(child);
-//        System.out.println("Child after mute: ");
-//        System.out.println(child.toString());
-//        return child;
         return mutate(new NQueenBoard(childBoard));
     }
 
-    // TODO: test mutating one values instead of multiple
     public NQueenBoard mutate(NQueenBoard child) {
         Random rand = new Random();
         double mutationRate = .01;
@@ -124,15 +98,6 @@ public class GeneticAlgorithm {
 
         return child;
     }
-
-    public NQueenBoard mutateOne(NQueenBoard child) {
-        Random rand = new Random();
-
-        child.moveQueenRandomly(rand.nextInt(child.getSize()));
-
-        return child;
-    }
-
 
     public NQueenBoard getSolutionBoard() {
         return solutionBoard;
