827adc20a520c2ef410459ee22be6962a2458ae0
Walt <wpniederer@cpp.edu>
2018-11-28 15:58:05 -0800

    added MovePairs class for keep track of moves

---
+++ Project 3/src/MovePairs.java
@@ -0,0 +1,21 @@
+public class MovePairs {
+    private String moveOne;
+    private String moveTwo;
+
+//    MoveTracker();
+//
+//    MoveTracker(int moveNumber, String moveOne, String moveTwo) {
+//        this.moveNumber = moveNumber;
+//        this.moveOne = moveOne;
+//        this.moveTwo = moveTwo;
+//    }
+
+
+    public void setMoveOne(String moveOne) { this.moveOne = moveOne; }
+
+    public void setMoveTwo(String moveTwo) { this.moveTwo = moveTwo; }
+
+    public String getMoveOne() { return moveOne; }
+
+    public String getMoveTwo() { return moveTwo; }
+}
